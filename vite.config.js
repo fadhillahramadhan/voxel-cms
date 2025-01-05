cdbb9dd983e8d6e39e8f15cdfb048b658acfbff6
fadhillahramadhan <fadhillahramadhan01@gmail.com>
2025-01-05 12:16:22 +0700

    refactor: update Vite configuration to include normalize.css for consistent styling

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
         laravel({
             input: [
                 "resources/js/app.js", // Main JS
+                // @vite('resources/assets/css/normalize.css')
+                "resources/assets/css/normalize.css",
             ],
             refresh: true, // Enable auto-reloading of assets during development
         }),
