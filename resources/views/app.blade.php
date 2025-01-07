d440cec27fc0d3ff4349689fc2ec3cffbbf487da
fadhillahramadhan <fadhillahramadhan01@gmail.com>
2025-01-08 05:33:53 +0700

    feat: update application title to 'Voxelize' for branding consistency

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Voxelize') }}</title>
     <!-- E:\Project\Javascript\Voxelize\VoxelEditorCMS\public\icons.png -->
     <link rel="icon" href="{{ asset('icons.png') }}" type="image/png">
     @vite('resources/js/app.js')
